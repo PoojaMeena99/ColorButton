10d4a7bf9ce7b88bb265de0431274409c9001411
PoojaMeena99 <110882484+PoojaMeena99@users.noreply.github.com>
2023-07-01 10:44:49 +0530

    Add files via upload

---
+++ Button_color.js
@@ -0,0 +1,23 @@
+let box = document.getElementById("abc")
+
+
+let redone =document.getElementById("red");
+redone.onclick=function(){
+    box.style.backgroundColor="red"
+}
+
+let bluetwo =document.getElementById("blue");
+bluetwo.onclick=function(){
+    box.style.backgroundColor="blue"
+}
+
+let yellowthree =document.getElementById("yellow");
+yellowthree.onclick=function(){
+    box.style.backgroundColor="yellow"
+}
+
+let pinkfour =document.getElementById("pink");
+pinkfour.onclick=function(){
+    box.style.backgroundColor="pink"
+}
+
